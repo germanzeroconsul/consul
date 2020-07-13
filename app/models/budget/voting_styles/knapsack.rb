09c12e04bc915168fecab3a72c9629084250ee3b
Javi Martín <javim@elretirao.net>
2020-07-13 02:11:24 +0200

    Extract methods showing voting style information
    The idea is that different voting styles will display different
    information messages.

--- app/models/budget/voting_styles/knapsack.rb
+++ app/models/budget/voting_styles/knapsack.rb
@@ -14,4 +14,12 @@ class Budget::VotingStyles::Knapsack < Budget::VotingStyles::Base
   def formatted_amount_available(heading)
     ballot.budget.formatted_amount(amount_available(heading))
   end
+
+  def formatted_amount_spent(heading)
+    ballot.budget.formatted_amount(amount_spent(heading))
+  end
+
+  def formatted_amount_limit(heading)
+    ballot.budget.formatted_heading_price(heading)
+  end
 end
